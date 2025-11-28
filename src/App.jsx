f8accd51b00f3ac2e28e81cd9989955156c65fe4
aashu <aashu@gmail.com>
2025-11-28 15:46:58 +0530

    Fix component import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Sidebar from "./components/Sidebar/Sidebar.jsx";
-import Main from "./components/Main/Main.jsx";
+import Sidebar from "./Components/Sidebar/Sidebar.jsx";
+import Main from "./Components/Main/Main.jsx";
 
 const App = () => {
   return (
